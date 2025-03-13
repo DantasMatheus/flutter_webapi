8b85f968c436a5f3398aab16b228df9d94febc99
MATHEUS DANTAS VIEIRA <ma_dv@outlook.com.br>
2025-03-13 16:03:54 -0300

    Style: alterações de fonte e tema do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +13,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Simple Journal',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(color: Colors.white),
+        ),
+        textTheme: GoogleFonts.bitterTextTheme(),
+      ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
       initialRoute: "home",
-      routes: {
-        "home": (context) => const HomeScreen(),
-      },
+      routes: {"home": (context) => const HomeScreen()},
     );
   }
 }

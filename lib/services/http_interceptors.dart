94f4a428694d1ec43456d132a0a84070429e14e7
MATHEUS DANTAS VIEIRA <ma_dv@outlook.com.br>
2025-03-18 09:38:32 -0300

    Add: Logger package

--- lib/services/http_interceptors.dart
+++ lib/services/http_interceptors.dart
@@ -1,14 +1,16 @@
 import 'package:http_interceptor/http_interceptor.dart';
-import 'dart:developer';
+import 'package:logger/logger.dart';
 
 class LoggerInterceptor extends InterceptorContract {
+  Logger logger = Logger();
+
   @override
   Future<BaseRequest> interceptRequest({required BaseRequest request}) async {
-    log('----- Request -----');
-    log('URL: ${request.url}');
-    log('Headers: ${request.headers}');
+    logger.t('----- Request -----');
+    logger.t('URL: ${request.url}');
+    logger.t('Headers: ${request.headers}');
     if (request is Request) {
-      log('Body: ${request.body}');
+      logger.t('Body: ${request.body}');
     }
     return request;
   }
@@ -17,11 +19,22 @@ class LoggerInterceptor extends InterceptorContract {
   Future<BaseResponse> interceptResponse({
     required BaseResponse response,
   }) async {
-    log('----- Response -----');
-    log('Status Code: ${response.statusCode}');
-    if (response is Response) {
-      log('Body: ${response.body}');
+    if (response.statusCode ~/ 100 == 2) {
+      logger.d(
+        "-------- Response --------\nStatus Code: ${response.statusCode}\n",
+      );
+      if (response is Response) {
+        logger.d('Body: ${response.body}');
+      }
+    } else {
+      logger.w(
+        "-------- Response --------\nStatus Code: ${response.statusCode}\nHeaders: ${response.headers}",
+      );
+      if (response is Response) {
+        logger.w('Body: ${response.body}');
+      }
     }
+
     return response;
   }
 }
